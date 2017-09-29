5a8689cfde8b6b5d362a25511bfd750135056489
fvh_P <ko_kurihara@yahoo.co.jp>
2017-09-29 12:18:03 +0900

    modify indent

--- ImasNews.rb
+++ ImasNews.rb
@@ -13,48 +13,48 @@ client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token:
 src = "http://idolmaster.jp/"
 
 begin
-    doc = Nokogiri::HTML(open(src))
+  doc = Nokogiri::HTML(open(src))
 rescue
-    f.puts()
-    f.puts(Time.now)
-    f.puts("アクセスできませんでした。\n#{src}")
-    f.puts(log[-2])
-    return
+  f.puts()
+  f.puts(Time.now)
+  f.puts("アクセスできませんでした。\n#{src}")
+  f.puts(log[-2])
+  return
 end
 
 news = doc.xpath('.//div[@class="clearfix"]').css("dd").css("a")
 post = Array.new
 
 (news.length - log.length).times do |i|
-    title = news[i].inner_html.split('】')
-    url = news[i].attribute('href').value
-    article = "【アイマスニュース】\n"
-    (title.length - 1).times do |i|
-        title[i] << "】"
-    end
-    title.each do |e|
-        article << "#{e}\n"
-    end
-    article << url
-    post << article
+  title = news[i].inner_html.split('】')
+  url = news[i].attribute('href').value
+  article = "【アイマスニュース】\n"
+  (title.length - 1).times do |i|
+    title[i] << "】"
+  end
+  title.each do |e|
+    article << "#{e}\n"
+  end
+  article << url
+  post << article
 end
 
 post.reverse.each do |e|
-    e << "\n#imas_news"
-    client.create_status(e)
+  e << "\n#imas_news"
+  client.create_status(e)
 end
 
 if post.length != 0
-    f.puts()
-    f.puts(Time.now)
-    f.puts(post)
+  f.puts()
+  f.puts(Time.now)
+  f.puts(post)
 end
 f.close
 
 if (news.length - log.length) != 0
-    f = File.open(File.expand_path("../ImasNewsLog.html", __FILE__),"w")
-    f.puts("<!DOCTYPE HTML><html>\n<head>\n<meta charset=\"utf-8\"/>\n<title>temp file</title></head>\n<body>")
-    f.puts(news)
-    f.puts("</body></html>")
-    f.close
+  f = File.open(File.expand_path("../ImasNewsLog.html", __FILE__),"w")
+  f.puts("<!DOCTYPE HTML><html>\n<head>\n<meta charset=\"utf-8\"/>\n<title>temp file</title></head>\n<body>")
+  f.puts(news)
+  f.puts("</body></html>")
+  f.close
 end
