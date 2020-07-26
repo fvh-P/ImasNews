dfe8ef1cbab2b97813d7b12cc1567ec06308ade6
fvh_P <ko_kurihara@yahoo.co.jp>
2020-07-27 01:13:56 +0900

    Redesign for renewal of the official website.

--- ImasNews.rb
+++ ImasNews.rb
@@ -17,13 +17,13 @@ rescue
   feed_list = []
 end
 
-log = Nokogiri::HTML(open(File.expand_path("../ImasNewsLog.html", __FILE__))).css("a")
+log = Nokogiri::HTML(open(File.expand_path("../ImasNewsLog.html", __FILE__))).xpath('.//div[@class="news-contents has-result"]/ul/li')
 f = File.open(File.expand_path("../ImasNews.log", __FILE__), "a")
 client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
-src = "http://idolmaster.jp/"
+src = "https://idolmaster-official.jp/news/"
 
 begin
-  doc = Nokogiri::HTML(open(src))
+  doc = Nokogiri::HTML(URI.open(src))
 rescue
   f.puts()
   f.puts(Time.now)
@@ -32,43 +32,23 @@ rescue
   return
 end
 
-news = doc.xpath('.//div[@class="clearfix"]').css("dd").css("a")
-post = Array.new
+news = doc.xpath('.//div[@class="news-contents has-result"]/ul/li')
 
-(news.length - log.length).times do |i|
-  title = news[i].inner_html.split('】')
-  url = news[i].attribute('href').value
-  article = "【アイマスニュース】\n"
-  (title.length - 1).times do |i|
-    title[i] << "】"
-  end
-  title.each do |e|
-    article << "#{e}\n"
-  end
-  article << url
-  post << article
-end
-
-post.reverse.each do |e|
-  client.create_status("#{e}\n#imas_news")
+news.each do |n|
+  title = n.xpath('.//div[@class="text-area"]/p[@class="text"]')&.inner_text
+  next if log.xpath('//div[@class="text-area"]/p[@class="text"]').map(&:inner_text).include?(title)
+  url = n.xpath('.//a')&.attribute("href")
+  post = "【アイマスニュース】\n#{title}\n#{url}"
+  client.create_status("#{post}\n#imas_news")
   feed_list.each do |id|
-    feed = "@#{client.account(id).acct} \n#{e}"
+    feed = "@#{client.account(id).acct} \n#{post}"
     client.create_status(feed, visibility: 'direct')
   end
-  e << "\n#imas_news"
-end
-
-if post.length != 0
   f.puts()
   f.puts(Time.now)
   f.puts(post)
 end
 f.close
 
-if (news.length - log.length) != 0
-  f = File.open(File.expand_path("../ImasNewsLog.html", __FILE__),"w")
-  f.puts("<!DOCTYPE HTML><html>\n<head>\n<meta charset=\"utf-8\"/>\n<title>temp file</title></head>\n<body>")
-  f.puts(news)
-  f.puts("</body></html>")
-  f.close
-end
+log_html = File.open(File.expand_path("../ImasNewsLog.html", __FILE__),"w")
+log_html.puts(doc)
